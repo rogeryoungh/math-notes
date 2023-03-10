8c616acd6ea50ab115bdf1f9af8e4334ae0a936a
rogeryoungh <rogeryoungh@foxmail.com>
2023-03-10 19:23:48 +0800

    update: reformat cls

---
+++ scripts/gen-book.sh
@@ -0,0 +1,10 @@
+cat style/code/purebook-base.cls   >    style/purebook.cls
+cat style/code/pure-font.cls       >>   style/purebook.cls
+cat style/code/pure-code.cls       >>   style/purebook.cls
+cat style/code/pure-thm-1.cls      >>   style/purebook.cls
+
+if [ "$1" = "--copy" ]; then
+  cp ./style/purebook.cls ./src/数学分析/purebook.cls
+  cp ./style/purebook.cls ./src/抽象代数/purebook.cls
+  cp ./style/purebook.cls ./src/高等代数/purebook.cls
+fi
